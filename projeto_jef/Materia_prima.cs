2e320ba185eb5f98e49f8e1c08fd5e2b713d6b99
mayara <mayara.santana46@sp.gov.sp.br>
2025-06-03 17:46:49 -0300

    criação de classes

---
+++ projeto_jef/Materia_prima.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projeto_jef
+{
+  public class Materia_prima
+    {
+        public int ID_Materia;
+        public string nome;
+        public decimal QuatidadeEstoque;
+        public string unidadeMedida;
+        public DateTime validade;
+        public int EstoqueAtual;
+    }
+}
